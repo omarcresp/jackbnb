edb01f589c3b64d8f2cd4e97bb2fc88dd1efa21f
Omar Crespo <crespomerchano@gmail.com>
2021-10-01 00:28:56 -0500

    direct mongo url

--- apps/jackbnb-ms/src/app.module.ts
+++ apps/jackbnb-ms/src/app.module.ts
@@ -6,26 +6,11 @@ import * as normalize from 'normalize-mongoose';
 import { AuthModule } from './modules/auth/auth.module';
 import { PropertiesModule } from './modules/properties/properties.module';
 
-const {
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  AWS_SESSION_TOKEN,
-  MONGO_CLUSTER,
-  MONGO_DATABASE,
-  MONGO_PARAMS,
-  IS_OFFLINE,
-  MONGO_URL,
-} = process.env;
-
-const mongoUri = `${MONGO_CLUSTER}.mongodb.net/${MONGO_DATABASE}?${MONGO_PARAMS}`
-const awsUrl = `mongodb+srv://${mongoUri}`;
-const mongoUrl = IS_OFFLINE === 'true' ? MONGO_URL : awsUrl;
-
-console.log('mongoUrl', mongoUrl);
+console.log('process.env.MONGO_URL', process.env.MONGO_URL);
 
 @Module({
   imports: [
-    MongooseModule.forRoot(encodeURIComponent(mongoUrl), {
+    MongooseModule.forRoot(process.env.MONGO_URL, {
       connectionFactory(connection) {
         connection.plugin(normalize);
 
