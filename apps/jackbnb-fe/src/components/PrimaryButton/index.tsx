5f1888266bed2157ba6bd9c77d63d206a524dca7
yasmin <yasmartins999@gmail.com>
2021-10-03 15:29:27 -0300

    añadir PrimaryButton

---
+++ apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+function PrimaryButton() {
+  return (
+    <div>
+
+     <Button colorScheme="#D81361" variant="solid" size="lg" w="150px" h="40px">
+    Boton
+  </Button>
+
+    </div>
+  );
+}
+
+export default PrimaryButton;
