9a4212975fda5f71ce29a5895926631929d568ff
Andres Aldana <andres.aldana.5@gmail.com>
2021-10-13 22:52:30 -0400

    properties service updated, dependencies defined, GET paginated properties method added.

--- microservices/properties-ms/src/properties/properties.service.ts
+++ microservices/properties-ms/src/properties/properties.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { paginate } from '@jackbnb/paginate';
+import { Model } from 'mongoose';
+
 import { CreatePropertyDto } from './dto/create-property.dto';
 import { UpdatePropertyDto } from './dto/update-property.dto';
+import { Property, PropertyDocument } from './entities/property.entity';
 
 @Injectable()
 export class PropertiesService {
+  constructor(
+    @InjectModel(Property.name) private PropertyModel: Model<PropertyDocument>,
+  ) {}
+
   create(createPropertyDto: CreatePropertyDto) {
     return 'This action adds a new property';
   }
 
-  findAll() {
-    return `This action returns all properties`;
+  async findAll(): Promise<PropertyDocument[]> {
+    const result = await this.PropertyModel.find().limit(300).exec();
+
+    if (!result) {
+      throw new NotFoundException();
+    }
+
+    const paginatedResults = paginate(result, 15);
+
+    return paginatedResults;
   }
 
   findOne(id: number) {
