63b4cec37dbd3025fc1007bcef746d5c0a6960c3
yasmin <yasmartins999@gmail.com>
2021-10-16 16:18:06 -0300

    add fitParent

--- apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
+++ apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from "react";
 import { Button } from "@chakra-ui/react";
 
+interface PrimaryButton {
+  fitParent?: boolean;
+}
+
 const PrimaryButton: FC = ({children}) => {
   return (
     <Button bg="#58DE15" h="50px" radio="10px" text-align="center" p="15px">
