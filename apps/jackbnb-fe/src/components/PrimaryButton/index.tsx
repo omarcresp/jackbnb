0128146447dcc26c5fc560298e672bfcc7025467
yasmin <yasmartins999@gmail.com>
2021-10-16 15:45:21 -0300

    boton

--- apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
+++ apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
@@ -3,13 +3,9 @@ import { Button } from "@chakra-ui/react";
 
 function PrimaryButton() {
   return (
-    <div>
-
-     <Button colorScheme="#D81361" variant="solid" size="lg" w="150px" h="40px">
-    Boton
-  </Button>
-
-    </div>
+    <Button bg="#58DE15" w="300px" h="50px">
+      Boton Primario
+    </Button>
   );
 }
 
