7f02ce14e2a1dac02c557376f0c3e3c13702de82
Yasmin Martins <87744245+tudulceprincesa18@users.noreply.github.com>
2021-10-16 20:06:28 -0300

    Rama yasmin (#5)
    * añadir PrimaryButton
    
    * boton
    
    * boton
    
    * add fitParent
    
    * add width
    
    * unset added
    
    * cambie el nombre de la cosa
    
    * ahi va

---
+++ apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import { Button } from "@chakra-ui/react";
+
+interface PrimaryButtonProps {
+  fitParent?: boolean;
+}
+
+const PrimaryButton: FC <PrimaryButtonProps> = ({children, fitParent}) => {
+  return (
+    <Button bg="#58DE15" h="50px" radio="10px" text-align="center" p="10px" width={fitParent ? 'inherit' : 'unset'}>
+      { children }
+    </Button>
+  );
+}
+
+export default PrimaryButton;
