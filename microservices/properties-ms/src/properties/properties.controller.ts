84dd4d3aa06d7b3402ba48283fa73735a1340044
Omar Crespo <crespomerchano@gmail.com>
2021-10-23 14:25:58 -0500

    remove controller preffix

--- microservices/properties-ms/src/properties/properties.controller.ts
+++ microservices/properties-ms/src/properties/properties.controller.ts
@@ -11,7 +11,7 @@ import { PropertiesService } from './properties.service';
 import { CreatePropertyDto } from './dto/create-property.dto';
 import { UpdatePropertyDto } from './dto/update-property.dto';
 
-@Controller('properties')
+@Controller()
 export class PropertiesController {
   constructor(private readonly propertiesService: PropertiesService) {}
 
