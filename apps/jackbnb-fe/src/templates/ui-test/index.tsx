1f6e64ff3345c17627e7adffd35ae43f3e515d96
rilp98 <63820704+rilp98@users.noreply.github.com>
2021-10-23 15:13:32 -0400

    resolve

--- apps/jackbnb-fe/src/templates/ui-test/index.tsx
+++ apps/jackbnb-fe/src/templates/ui-test/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import type { NextPage } from 'next'
+import { Stack } from "@chakra-ui/react";
+import IconButton from '../../components/IconButton';
+import iconApple from './assets/icon/apple.svg';
+import iconEmail from './assets/icon/email.svg';
+import iconFacebook from './assets/icon/facebook.svg';
+import iconGoogle from './assets/icon/google.svg';
+
 
 import MyTestButton from '../../components/MyTestButton';
 
@@ -9,6 +16,26 @@ const UiTestPage: NextPage = () => {
       <h1>Hello</h1>
 
       <MyTestButton>World</MyTestButton>
+
+      <Stack spacing={4} mt={8} ml={8} >
+
+      <IconButton icon={iconApple}>
+        Registrate en Apple
+      <IconButton/>
+  
+      <IconButton icon={iconFacebook}>
+        Registrate en Facebook
+      </IconButton>
+  
+      <IconButton icon={iconGoogle}>
+        Registrate en Google
+      <IconButton/>
+
+      <IconButton icon={iconEmail}>
+        Registrate por correo
+      <IconButton/>
+  
+  </Stack>
     </div>
   )
 };
