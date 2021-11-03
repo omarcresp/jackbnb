65183cce54af4bba4d7014ce3f4a34a01ef3b297
Andres Aldana <andres.aldana.5@gmail.com>
2021-11-03 16:20:40 -0400

    Unit Test update

--- microservices/properties-ms/src/properties/properties.controller.spec.ts
+++ microservices/properties-ms/src/properties/properties.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+
 import { PropertiesController } from './properties.controller';
 import { PropertiesService } from './properties.service';
+import { Property } from './entities/property.entity';
 
 describe('PropertiesController', () => {
   let controller: PropertiesController;
@@ -8,7 +11,13 @@ describe('PropertiesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PropertiesController],
-      providers: [PropertiesService],
+      providers: [
+        PropertiesService,
+        {
+          provide: getModelToken(Property.name),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<PropertiesController>(PropertiesController);
--- microservices/properties-ms/src/properties/properties.service.spec.ts
+++ microservices/properties-ms/src/properties/properties.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+
 import { PropertiesService } from './properties.service';
+import { Property } from './entities/property.entity';
 
 describe('PropertiesService', () => {
   let service: PropertiesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PropertiesService],
+      providers: [
+        PropertiesService,
+        {
+          provide: getModelToken(Property.name),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<PropertiesService>(PropertiesService);
