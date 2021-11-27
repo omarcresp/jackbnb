2aab71e79f8deaee27b1dc2d726852bddb0438c8
rilp98 <paolaanttonelar23@gmail.com>
2021-11-27 15:36:36 -0400

    Changes to SpaceType component

--- apps/jackbnb-fe/src/components/SpaceType/index.tsx
+++ apps/jackbnb-fe/src/components/SpaceType/index.tsx
@@ -30,27 +30,27 @@ const SpaceType: FC = () => {
 
   return (
 
-    <div className={SpaceTypeStyles.myContainer_item}>
+    <div className={SpaceTypeStyles.myContainer}>
 
-      <Box mt="47px" mb="24px">
+        <Box mt="47px" mb="24px">
 
-          <Heading as="h2" size="22px" color="#000000" textAlign="center" >¿Qué espacio estás buscando?</Heading>
+            <Heading as="h2" size="22px" color="#000000" textAlign="center" >¿Qué espacio estás buscando?</Heading>
 
-      </Box>
+        </Box>
 
-      <div className={SpaceTypeStyles.myContainer} >
-          
-          <CirclesImage alt="zona verde" textImage="Zonas Verdes" />
-          
-          <CirclesImage alt="zona urbana" textImage="Zonas Urbanas"/>
+        <div className={SpaceTypeStyles.myContainer_items} >
+            
+            <CirclesImage alt="zona verde" textImage="Zonas Verdes" />
+            
+            <CirclesImage alt="zona urbana" textImage="Zonas Urbanas"/>
 
-          <CirclesImage alt="biblioteca" textImage="Biblioteca"/>
+            <CirclesImage alt="biblioteca" textImage="Biblioteca"/>
 
-          <CirclesImage alt="oficina" textImage="Oficina"/>
+            <CirclesImage alt="oficina" textImage="Oficina"/>
 
-          <CirclesImage alt="coworking" textImage="Coworking"/>
+            <CirclesImage alt="coworking" textImage="Coworking"/>
 
-      </div>
+        </div>
 
     </div>
 
