e36f4f9ebf08d754fc5488f98c49fb678c9fa476
Omar Crespo <crespomerchano@gmail.com>
2021-11-27 15:23:05 -0500

    add eslint to frontend and run eslint fix

--- apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
+++ apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
@@ -1,16 +1,26 @@
-import React, { FC } from "react";
-import { Button } from "@chakra-ui/react";
+import React, {FC} from 'react';
+import {Button} from '@chakra-ui/react';
 
 interface PrimaryButtonProps {
   fitParent?: boolean;
 }
 
-const PrimaryButton: FC <PrimaryButtonProps> = ({children, fitParent}) => {
+const PrimaryButton: FC<PrimaryButtonProps> = ({children, fitParent}) => {
   return (
-    <Button bg="#58DE15" h="50px" radio="10px" text-align="center" p="10px" width={fitParent ? 'inherit' : 'unset'}>
-      { children }
+    <Button
+      bg="#58DE15"
+      h="50px"
+      radio="10px"
+      text-align="center"
+      p="10px"
+      width={fitParent ? 'inherit' : 'unset'}>
+      {children}
     </Button>
   );
-}
+};
+
+PrimaryButton.defaultProps = {
+  fitParent: false,
+};
 
 export default PrimaryButton;
--- apps/jackbnb-fe/src/pages/_app.tsx
+++ apps/jackbnb-fe/src/pages/_app.tsx
@@ -1,13 +1,15 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import React, {FC} from 'react';
+import {ChakraProvider} from '@chakra-ui/react';
+import type {AppProps} from 'next/app';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const AppComponent: FC<AppProps> = ({Component, pageProps}) => {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
-}
-export default MyApp
+  );
+};
+
+export default AppComponent;
