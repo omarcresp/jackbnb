57f1b5e464de71d9c48d93335320d12d176cc1b8
rilp98 <paolaanttonelar23@gmail.com>
2021-11-29 13:41:38 -0400

    Adding ESLint

--- apps/jackbnb-fe/src/components/IconButton/index.tsx
+++ apps/jackbnb-fe/src/components/IconButton/index.tsx
@@ -1,29 +1,35 @@
-import React, { FC } from 'react';
-import { Button, Img, Box, Divider } from '@chakra-ui/react';
+import React, {FC} from 'react';
+import {Button, Img, Box, Divider} from '@chakra-ui/react';
 
-interface IconButtonProps{
-  icon:string,
-  marginLeft:string,
-  title:string;
+interface IconButtonProps {
+  icon: string;
+  marginLeft: string;
+  title: string;
 }
 
-const IconButton: FC<IconButtonProps> = ({icon,marginLeft,title}) => {
+const IconButton: FC<IconButtonProps> = ({icon, marginLeft, title}) => {
   return (
     <>
-      <Button width="375px" height="50px" borderColor="#808080" variant="outline" fontSize="18px" color="#136262"
-        
-        justifyContent={'flex-start'} >
-     
-        <Img src={icon}/>
+      <Button
+        width="375px"
+        height="50px"
+        borderColor="#808080"
+        variant="outline"
+        fontSize="18px"
+        color="#136262"
+        justifyContent="flex-start">
+        <Img src={icon} />
 
-          <Box height="30px" >
+        <Box height="30px">
+          <Divider
+            orientation="vertical"
+            borderColor="#808080"
+            mr="10px"
+            ml={marginLeft}
+          />
+        </Box>
 
-            <Divider orientation="vertical" borderColor="#808080" mr="10px" ml={marginLeft} />
-
-          </Box>
-
-           {title}
-
+        {title}
       </Button>
     </>
   );
--- apps/jackbnb-fe/src/components/SocialButton/index.tsx
+++ apps/jackbnb-fe/src/components/SocialButton/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { Stack } from '@chakra-ui/react';
+import React, {FC} from 'react';
+import {Stack} from '@chakra-ui/react';
 
 import emailIcon from '../../assets/icon/email.svg';
 import googleIcon from '../../assets/icon/google.svg';
@@ -8,28 +8,36 @@ import appleIcon from '../../assets/icon/apple.svg';
 
 import IconButton from '../IconButton';
 
-
-
 const SocialButton: FC = () => {
-    
-    return (
+  return (
     <>
+      <Stack align="center" spacing="21px">
+        <IconButton
+          title="Ingresa por correo electrónico"
+          icon={emailIcon.src}
+          marginLeft="13px"
+        />
 
-    <Stack align="center" spacing="21px">
+        <IconButton
+          title="Continúa con Google"
+          icon={googleIcon.src}
+          marginLeft="17px"
+        />
 
-    <IconButton title="Ingresa por correo electrónico" icon={emailIcon.src} marginLeft="13px" />
+        <IconButton
+          title="Continúa con Facebook"
+          icon={facebookIcon.src}
+          marginLeft="13px"
+        />
 
-    <IconButton title="Continúa con Google" icon={googleIcon.src} marginLeft="17px" />
+        <IconButton
+          title="Continúa con Apple"
+          icon={appleIcon.src}
+          marginLeft="17px"
+        />
+      </Stack>
+    </>
+  );
+};
 
-    <IconButton title="Continúa con Facebook" icon={facebookIcon.src} marginLeft="13px" />
-
-    <IconButton title="Continúa con Apple" icon={appleIcon.src} marginLeft="17px" />
-
-    </Stack>
-
-     </>
-    );
-  };
-  
-  export default SocialButton;
-    
+export default SocialButton;
