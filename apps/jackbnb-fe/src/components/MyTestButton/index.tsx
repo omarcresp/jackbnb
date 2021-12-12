144a4b064afbee70dc487d5b679688c5a37d4f46
Omar Crespo <crespomerchano@gmail.com>
2021-12-12 10:36:28 -0500

    update test

--- apps/jackbnb-fe/src/components/MyTestButton/index.tsx
+++ apps/jackbnb-fe/src/components/MyTestButton/index.tsx
@@ -3,11 +3,16 @@ import {Button} from '@chakra-ui/react';
 
 import MyTestButtonStyles from './index.module.css';
 
-const MyTestButton: FC = ({children}) => {
+interface MyTestButtonProps {
+  onClick: () => void;
+}
+
+const MyTestButton: FC<MyTestButtonProps> = ({children, onClick}) => {
   const [message, setMessage] = useState('');
 
   const sayHi = () => {
     setMessage('hidden message');
+    onClick?.();
   };
 
   return (
