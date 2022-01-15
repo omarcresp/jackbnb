94cb89662b559cd145e0dbe477c5827888d353c0
yasmin <yasmartins999@gmail.com>
2022-01-15 11:35:01 -0300

    el onclick estaba dos veces

--- apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
+++ apps/jackbnb-fe/src/components/PrimaryButton/index.tsx
@@ -14,7 +14,6 @@ const PrimaryButton: FC<PrimaryButtonProps> = ({children, fitParent, onClick}) =
       radio="10px"
       text-align="center"
       p="10px"
-      onClick={onClick}
       width={fitParent ? 'inherit' : 'unset'}
       onClick={onClick}>
       Continuar
