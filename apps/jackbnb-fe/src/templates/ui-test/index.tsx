8e2537de658426cf3605abd0264a7b66d30358e1
rilp98 <paolaanttonelar23@gmail.com>
2022-01-19 16:15:02 -0400

    Delete MyTestButton component

--- apps/jackbnb-fe/src/templates/ui-test/index.tsx
+++ apps/jackbnb-fe/src/templates/ui-test/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type {NextPage} from 'next';
 
-import MyTestButton from '../../components/MyTestButton';
 import NavBar from '../../components/NavBar';
 import SpaceType from '../../components/SpaceType';
 import Benefits from '../../components/Benefits';
@@ -9,9 +8,6 @@ import Benefits from '../../components/Benefits';
 const UiTestPage: NextPage = () => {
   return (
     <>
-      <h1>Hello</h1>
-
-      <MyTestButton>World</MyTestButton>
       <NavBar />
       <SpaceType />
       <Benefits />
