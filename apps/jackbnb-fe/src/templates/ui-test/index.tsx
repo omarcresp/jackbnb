dc3125c35ae6ecc33dce20f3552d621063871fbd
yasmin <yasmartins999@gmail.com>
2022-01-19 22:22:02 -0300

    new changes

--- apps/jackbnb-fe/src/templates/ui-test/index.tsx
+++ apps/jackbnb-fe/src/templates/ui-test/index.tsx
@@ -4,15 +4,19 @@ import Card from '../../components/Card'
 import Footer from '../../components/Footer'
 import PrimaryButton from '../../components/PrimaryButton'
 import Header from '../../components/Header'
+import FormButton from '../../components/SecondaryButton';
 
 
 const UiTestPage: NextPage = () => {
+  const click = () => {
+    console.log('oli')
+  }
   return (
     <div>
       <Footer />
       <Card />
-      <PrimaryButton children={'Continuar'} fitParent={true}/>
-      <FormButton  children={'hola'}  />
+      <PrimaryButton children={'Continuar'} fitParent={true} onClick={click} />
+      <FormButton  children={'hola'}/>
       <Header />
     </div>
   );
