9c14e2cfe7e82787ac7b1f459680632b9ab91eca
Paola Rossi <63820704+rilp98@users.noreply.github.com>
2022-01-27 23:24:16 -0400

    Adding the NavBar, SpaceType and Benefits components (#9)
    * Adding the navBar, spaceType and Benefits components
    
    * Correct errors in the navBar, SpaceType and Benefits, in addition to typing the components
    
    * Changes to SpaceType component
    
    * Adding Items component
    
    * Adding Items and CirleImage components
    
    * Adding ESlint
    
    * Improving the code
    
    * Adding arry with map for the UI
    
    * Improving the SpaceType and Benefits components
    
    * Improving the SpaceType and Benefits components
    
    * Improving SpaceType, NavBar and Benefits components
    
    * Improving the code
    
    * Delete MyTestButton component
    
    Co-authored-by: omar crespo <crespomerchano@gmail.com>

--- apps/jackbnb-fe/src/templates/ui-test/index.tsx
+++ apps/jackbnb-fe/src/templates/ui-test/index.tsx
@@ -1,38 +1,18 @@
 import React from 'react';
 import type {NextPage} from 'next';
-import Card from '../../components/Card';
-import Footer from '../../components/Footer';
-import PrimaryButton from '../../components/PrimaryButton';
-import Header from '../../components/Header';
-import FormButton from '../../components/SecondaryButton';
+
+import NavBar from '../../components/NavBar';
+import SpaceType from '../../components/SpaceType';
+import Benefits from '../../components/Benefits';
 
 const UiTestPage: NextPage = () => {
-  const click = () => {
-    console.log('oli');
-  };
-
   return (
-    <div>
-      <Footer />
-      <Card />
-      <PrimaryButton fitParent onClick={click}>
-        Continuar
-      </PrimaryButton>
-      <FormButton width onClick={click}>
-        hola
-      </FormButton>
-      <Header fitParent />
-    </div>
+    <>
+      <NavBar />
+      <SpaceType />
+      <Benefits />
+    </>
   );
-};
-
-/**
- * FIXME: This is a workaround to avoid the error:
- * Error occurred pre-rendering page "/ui-test".
- * TypeError: Cannot read properties of undefined (reading 'onToggle')
- */
-UiTestPage.getInitialProps = async () => {
-  return {};
 };
 
 export default UiTestPage;
